ad455e347ac02c95a9f1244624234e2a8b78b6c1
Sixie Yu <sixie.yu@wustl.edu>
2023-09-21 13:49:03 -0700

    Update baselines_run.sh
    fix the save_dir in baselines_run.sh

--- src/UnlearnSparse/baselines_run.sh
+++ src/UnlearnSparse/baselines_run.sh
@@ -7,10 +7,10 @@ mkdir -p $output_dir
 counter=0
 for seed in {1..10}
 do
-    # python -u main_forget.py --save_dir results --unlearn FT  --unlearn_lr 0.01 --unlearn_epochs 10 --gpu 1  --seed ${seed} &
-    # python -u main_forget.py --save_dir results --unlearn GA  --unlearn_lr 0.0001 --unlearn_epochs 5 --gpu 2 --seed ${seed} &
-    # python -u main_forget.py --save_dir results --unlearn fisher_new --alpha ${alpha} --gpu 0 --seed ${seed} &
-    # python -u main_forget.py --save_dir results --unlearn wfisher --num_indexes_to_replace 4500 --alpha ${alpha} --gpu 2 --seed ${seed} &
+    # python -u main_forget.py --save_dir=${output_dir} --unlearn FT  --unlearn_lr 0.01 --unlearn_epochs 10 --gpu 1  --seed ${seed} &
+    # python -u main_forget.py --save_dir=${output_dir} --unlearn GA  --unlearn_lr 0.0001 --unlearn_epochs 5 --gpu 2 --seed ${seed} &
+    # python -u main_forget.py --save_dir=${output_dir} --unlearn fisher_new --alpha ${alpha} --gpu 0 --seed ${seed} &
+    # python -u main_forget.py --save_dir=${output_dir} --unlearn wfisher --num_indexes_to_replace 4500 --alpha ${alpha} --gpu 2 --seed ${seed} &
     python -u main_forget.py --save_dir=${output_dir} --unlearn retrain --unlearn_epochs 160 --unlearn_lr 0.1 --gpu 2 --seed ${seed} &
 
     counter=$((counter+1))
