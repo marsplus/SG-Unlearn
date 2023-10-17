fix the coeff. for gradient accumulation

src/models.py
@@ -193,9 +193,16 @@ class DefenderOPT(nn.Module):
                 ## _set_lr enables different lr for the defender and attacker
                 self._set_lr(optimizer, new_lr=self.attacker_lr) 
                 optimizer.zero_grad()
+                ## the gradient of the attacker's optimization w.r.t. the model parameters:
+                ## Let N = #(forget data), B=batch size
+                ## batched version (memory intensive): (1/N) * (dL / dw)
+                ## mini batch version (memory friendly): (1/B) * (dL / dw), so we need to multiply by a factor of (B/N)
+                N = len(self.forget_loader.dataset)
                 for (forget_data, forget_targets), (val_data, val_targets) in zip(self.forget_loader, self.val_loader):
+                    ## this is inside the loop because the batch sizes may be different from each other
+                    B = forget_data.shape[0]
                     att_lik, att_acc, _ = self._attacker_opt(forget_data, forget_targets, val_data, val_targets, net)
-                    u_a = att_lik * self.attacker_strength
+                    u_a = att_lik * self.attacker_strength * (B/N)
                     ## the defender wants to minimize the attacker's utility u_a
                     u_a.backward()
                 optimizer.step()