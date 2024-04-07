dbc910351a28a9d9ab94b31037cfddca461cd290
marsplus <sixie.yu@wustl.edu>
2024-04-07 10:59:27 -0700

    add instructions to use

--- src/eval.sh
+++ src/eval.sh
@@ -1,13 +1,22 @@
+#!/bin/bash
 
+usage="Usage: $0 <method> <dataset> <device_id>\n
+<method>: The method to evaluate. Options include 'SG', 'retrain', etc.\n
+<dataset>: The dataset to evaluate on. Example datasets: 'cifar10', 'cifar100'.\n
+<device_id>: The device ID to run the evaluation on. For example, '0' for the first GPU.\n"
 
-# method=$1
-# dataset=$2
-dataset=$1
-device_id=$2
+## Example usage: ./eval.sh SG cifar10 0; this evaluates SG on cifar10 using GPU 0
+
+# Check if exactly three arguments are given, else print the usage
+if [ "$#" -ne 3 ]; then
+    echo -e $usage
+    exit 1
+fi
+
+method=$1
+dataset=$2
+device_id=$3
 checkpoint_path="../result/SG_diff_strength_mini_batch"
 
-for method in SG
-do
-    echo $method
-    python evaluate.py --dataset ${dataset} --checkpoint_path ${checkpoint_path} --method ${method} --device_id ${device_id}
-done
+echo $method
+python evaluate.py --dataset ${dataset} --checkpoint_path ${checkpoint_path} --method ${method} --device_id ${device_id}
