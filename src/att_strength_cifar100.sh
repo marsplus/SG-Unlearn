0a4a0dc21c49797ff422d42600cce55eb4242cb1
marsplus <sixie.yu@wustl.edu>
2024-04-08 11:00:10 -0700

    add the shell scripts to run experiments

---
+++ src/att_strength_cifar100.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ne=10
+cv=3
+d=100
+clas=SVM
+with_attacker=1
+dataset="cifar100"
+output_dir="../result/SG_diff_strength_mini_batch/${dataset}"
+mkdir -p $output_dir
+
+# atts=$2
+# for atts in 0.05 0.1 0.25 0.5 1 2 5 0
+for atts in 1
+do  
+    for seed in {1..10}
+    do  
+        python main.py \
+            --device_id=$1 \
+            --num_epoch=$ne \
+            --cv=$cv \
+            --dim=$d \
+            --output_dir=$output_dir \
+            --seed=$seed \
+            --dataset=$dataset \
+            --save_checkpoint=1 \
+            --attacker_strength=$atts \
+            --mem_save=10 \
+            --baseline_mode=0
+    done
+done
