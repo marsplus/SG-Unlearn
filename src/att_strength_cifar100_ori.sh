372d322ee854887ad1678684062246f7df857702
ElegantLin <elegant.lin21@gmail.com>
2024-04-13 22:59:12 +0000

    reformat the code

--- src/att_strength_cifar100_ori.sh
+++ src/att_strength_cifar100_ori.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 ne=30
 cv=3
-d=10
+d=100
 clas=SVM
 with_attacker=1
-dataset="cifar10"
+dataset="cifar100"
 output_dir="../result/SG_diff_strength_mini_batch/${dataset}"
 mkdir -p $output_dir
 
@@ -12,15 +13,16 @@ mkdir -p $output_dir
 # for atts in 0.05 0.1 0.25 0.5 1 2 5 0
 for atts in 1
 do  
-    for seed in {6..10}
+    for seed in {1..10}
     do  
         python main.py \
-            --device_id=$1 \
+            --device_id="$1" \
             --num_epoch=$ne \
+            --num_class=100 \
             --cv=$cv \
             --dim=$d \
             --output_dir=$output_dir \
-            --seed=$seed \
+            --seed="$seed" \
             --dataset=$dataset \
             --save_checkpoint=1 \
             --attacker_strength=$atts \
